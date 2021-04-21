1cea0c6730622af4b9707d9565296bdcad11f029
jungwoo Lee <b5nnnal88@gmail.com>
2021-04-22 08:19:50 +0900

    update todolist tableView

--- TodayList/TodayList/ViewController.swift
+++ TodayList/TodayList/ViewController.swift
@@ -8,11 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var tableView:UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+    
+  
 
 
 }
