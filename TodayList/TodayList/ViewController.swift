a61bb67c7dfeb33482aac4bfcbe08706aa1b6617
jungwoo Lee <b5nnnal88@gmail.com>
2021-05-01 17:21:38 +0900

    update data in todocell

--- TodayList/TodayList/ViewController.swift
+++ TodayList/TodayList/ViewController.swift
@@ -7,15 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate,UITableViewDataSource {
     
+    @IBOutlet weak var tableView : UITableView!
     
+    let cellIdentifier : String = "todocell"
     
-    var titletext : [String] = []
+    let todayToDo : [String] = ["빨래","swift 공부","철권"]
     
-    @IBAction func touchUpAddButton(_ sender: UIButton) {
-        
-    }
+    let weekToDO : [String] = ["술마시러가기","todolist 앱 개발","구체적인 기획서 작성하기"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,9 +24,36 @@ class ViewController: UIViewController {
         
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return todayToDo.count
+        case 1:
+            return weekToDO.count
+        default:
+            return 0
+        }
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
+        
+        let text: String = indexPath.section == 0 ? todayToDo[indexPath.row] : weekToDO[indexPath.row]
+        
+        cell.textLabel?.text = text
+        
+        
+        return cell
+    }
   
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return section == 0 ? "오늘 할 일" : "이번주 할 일"
+    }
 
 
 }
